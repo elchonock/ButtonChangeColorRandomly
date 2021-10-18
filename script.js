9eed751ab5f1a03c82a237f854fafd12a51752b3
Elvira <elya3029@gmail.com>
2021-10-18 23:59:52 +0300

    add e

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded',()=> {
     });
 
     function getRandomColor() {
-        const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'f'];
+        const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
         const color = [];
         let colorStr;
         for (let i = 0; i<6; i++) {
